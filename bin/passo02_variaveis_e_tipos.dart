f661bd719c415b236e48aaa356d3f5acc76b68dc
Maxx <216940663+sout-MMdev@users.noreply.github.com>
2026-09-24 11:53:32 -0300

    Passo 2 concluído: variáveis e tipos

--- bin/passo02_variaveis_e_tipos.dart
+++ bin/passo02_variaveis_e_tipos.dart
@@ -19,5 +19,23 @@
 // Digite à mão, faça antes de abrir a solução no guia.
 
 void main() {
-  // Seu código aqui.
+  const nomeDaLoja = 'Lojas DevMax';
+  // O produto é sempre esse nesse lote
+  const String produto = 'Iphone 16 PROMAX';
+  // o valor do produto não vai mudar no momento e já com desconto esse valor.
+  final double preco = 9900.00;
+  // a quantidade pode alterar caso o cliente queria mais um
+  var quantidade = 1;
+  // sempre vai ter desconto nesse produto
+  final bool temDesconto = true;
+
+  print('O nome da loja se chama $nomeDaLoja');
+
+  print('O produto que estão vendendo é um $produto');
+
+  print('O valor do produto é: R\$ ${preco.toStringAsFixed(2)}');
+
+  print('Quantidade de produto escolhido: $quantidade');
+
+  print('Esse produto tem desconto: $temDesconto');
 }
